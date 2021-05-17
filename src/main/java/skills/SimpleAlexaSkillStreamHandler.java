a24800c249d7ed5bc691f0dc6b4a8521eeac5d34
lucas_galo <lucas_d>
2021-05-17 09:09:04 -0300

    Colocando as referencias

--- src/main/java/skills/SimpleAlexaSkillStreamHandler.java
+++ src/main/java/skills/SimpleAlexaSkillStreamHandler.java
@@ -2,7 +2,8 @@ package skills;
 
 import com.amazon.ask.SkillStreamHandler;
 import com.amazon.ask.Skills;
-
+// referencias
+//https://www.youtube.com/watch?v=5rHIHUjuNRk
 public class SimpleAlexaSkillStreamHandler extends SkillStreamHandler {
 
     public SimpleAlexaSkillStreamHandler() {
